48b32515623519b3a951255a45e21a985b45920e
Hamza Ghous <58989094+humzaghous@users.noreply.github.com>
2024-07-10 10:53:33 +0500

    Update terra.tf

--- tffile/terra.tf
+++ tffile/terra.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "my_vpc"
   }
@@ -12,21 +13,18 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
+
   tags = {
     Name = "my_subnet"
   }
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID, replace with a valid AMI ID for your region
+  ami           = "ami-06c68f701d8090592"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_subnet.id
 
   tags = {
     Name = "my_instance"
   }
-}
-
-output "instance_id" {
-  value = aws_instance.my_instance.id
 }
